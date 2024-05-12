ba55cbffd49e8e4819e2a2891f12552bb69331ee
BreakZero <dejin.po@gmail.com>
2024-05-12 19:32:14 +0800

    remove flovar in library plugin

--- convention/src/main/kotlin/org/easy/mobile/convention/AndroidBuildConfig.kt
+++ convention/src/main/kotlin/org/easy/mobile/convention/AndroidBuildConfig.kt
@@ -4,6 +4,4 @@ object AndroidBuildConfig {
     const val compileSdkVersion = 34
     const val minSdkVersion = 28
     const val targetSdkVersion = 34
-    const val versionCode = 10002
-    const val versionName = "v1.0.2"
 }
