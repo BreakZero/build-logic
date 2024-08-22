a945cc9630a137c7121af5cf95ac8e61cffab4e7
= <dejin.po@gmail.com>
2024-08-22 10:42:26 +0800

    clean style

--- convention/src/main/kotlin/org/easy/mobile/convention/AndroidBuildConfig.kt
+++ convention/src/main/kotlin/org/easy/mobile/convention/AndroidBuildConfig.kt
@@ -10,3 +10,18 @@ object AndroidBuildConfig {
     const val minSdkVersion = 28
     const val targetSdkVersion = 34
 }
+
+/**
+ * @param path 文件路径，相对于rootProject的路径
+ */
+fun Project.getPropertiesByFile(path: String): Properties {
+    val properties = Properties()
+    val keyPropertiesFile = rootProject.file(path)
+
+    if (keyPropertiesFile.isFile) {
+        InputStreamReader(FileInputStream(keyPropertiesFile), Charsets.UTF_8).use { reader ->
+            properties.load(reader)
+        }
+    }
+    return properties
+}
