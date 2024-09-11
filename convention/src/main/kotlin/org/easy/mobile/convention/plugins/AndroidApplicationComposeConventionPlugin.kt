c2c388691b977e7f0842c6acfdb7dc13d7860e1a
= <dejin.po@gmail.com>
2024-09-11 14:14:21 +0800

    Fix Application lack to apply compose plugin

--- convention/src/main/kotlin/org/easy/mobile/convention/plugins/AndroidApplicationComposeConventionPlugin.kt
+++ convention/src/main/kotlin/org/easy/mobile/convention/plugins/AndroidApplicationComposeConventionPlugin.kt
@@ -4,14 +4,14 @@ import com.android.build.api.dsl.ApplicationExtension
 import org.easy.mobile.convention.configureAndroidCompose
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.getByType
 
 class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            with(pluginManager) {
-                apply("com.android.application")
-            }
+            apply(plugin = "com.android.application")
+            apply(plugin = "org.jetbrains.kotlin.plugin.compose")
 
             val extension = extensions.getByType<ApplicationExtension>()
 
